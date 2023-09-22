044dbb6d29a6b326d77dfaec82d073f0d42c15c3
Brij Bhatia <brijbhatia997@gmail.com>
2023-09-22 11:32:16 -0700

    first commit

---
+++ LittleLemon/myapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Booking
+class MenuForm(forms.Form):
+    item_name = forms.CharField(max_length = 200)
+    category = forms.CharField(max_length = 200)
+    description = forms.CharField(max_length = 1000)
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = "__all__"
