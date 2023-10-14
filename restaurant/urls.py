5960f985ed6d028a43145296ac88a8efe0897431
Brij Bhatia <brijbhatia997@gmail.com>
2023-10-14 03:33:17 -0700

    Added Serializers and updated routing.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router=DefaultRouter()
+router.register(r'tables', viewset=views.BookingViewSet,basename='booking')
+
 urlpatterns = [
-    path('menu/', views.MenuItemView.as_view()),
+    path('',views.home,name='home'),
+    path('menu/', views.MenuItemView.as_view(),name="menu"),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
