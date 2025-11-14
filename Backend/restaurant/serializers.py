0676f406c928da8a45f1faaeca1fd96975ecd703
Brij Bhatia <brijbhatia997@gmail.com>
2025-11-14 17:06:00 +0530

    Segregated Backend Code and Updated Requirements

---
+++ Backend/restaurant/serializers.py
@@ -0,0 +1,18 @@
+from .models import Booking,Menu
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Menu
+        fields=['id','title','price','inventory']
+        
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Booking
+        fields='__all__'
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['url', 'username', 'email', 'groups']
